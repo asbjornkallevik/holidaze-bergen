bc83c2041264dc53017d1f9f87e176a23a83e58f
asbjornkallevik <88311284+asbjornkallevik@users.noreply.github.com>
2022-01-08 19:45:08 +0100

    Create endpoint variables

--- constants/api.js
+++ constants/api.js
@@ -1,4 +1,7 @@
 export const API_URL = process.env.API_URL;
 export const API_BASE_URL = process.env.API_BASE_URL;
 
+export const ACCOMMODATION_ENDPOINT = "accommodation/";
+export const MEDIA_ENDPOINT = "media/";
+
 export const TOKEN_PATH = "jwt-auth/v1/token";
