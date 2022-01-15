6251671228aa24b6e03eba4c8411988fd0d891f2
asbjornkallevik <88311284+asbjornkallevik@users.noreply.github.com>
2022-01-15 14:40:53 +0100

    Nav active class

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,8 +1,46 @@
+import { useRouter } from "next/router";
 import Logo from "./Logo";
 import HamburgerMenu from "./HamburgerMenu";
 import Link from "next/link";
 
-function Header() {
+export default function Header() {
+  const router = useRouter();
+
+  // Create menu items and assign classes
+  const menuItems = [
+    {
+      slug: "",
+      pageName: "Home",
+      itemClass: router.pathname == "/" ? "menu__item active" : "menu__item",
+    },
+    {
+      slug: "accommodation",
+      pageName: "Accommodation",
+      itemClass:
+        router.pathname == "/accommodation"
+          ? "menu__item active"
+          : "menu__item",
+    },
+    {
+      slug: "about-us",
+      pageName: "About us",
+      itemClass:
+        router.pathname == "/about-us" ? "menu__item active" : "menu__item",
+    },
+    {
+      slug: "contact",
+      pageName: "Contact",
+      itemClass:
+        router.pathname == "/contact" ? "menu__item active" : "menu__item",
+    },
+    {
+      slug: "login",
+      pageName: "Log in",
+      itemClass:
+        router.pathname == "/login" ? "menu__item active" : "menu__item",
+    },
+  ];
+
   return (
     <header className="page-header">
       <HamburgerMenu />
@@ -15,15 +53,14 @@ function Header() {
       <div className="page-header__nav-wrapper">
         <nav>
           <ul className="page-header__menu menu">
-            <li className="menu__item">
-              <Link href="/">Home</Link>
-            </li>
-            <li className="menu__item">
-              <Link href="/accommodation">Accommodation</Link>
-            </li>
-            <li className="menu__item">About us</li>
-            <li className="menu__item">Contact</li>
-            <li className="menu__item">Log in</li>
+            {/* Display menu items */}
+            {menuItems.map((item) => {
+              return (
+                <li key={item.slug} className={item.itemClass}>
+                  <Link href={`/${item.slug}`}>{item.pageName}</Link>
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </div>
@@ -31,5 +68,3 @@ function Header() {
     </header>
   );
 }
-
-export default Header;
