e88bf2cedea9adc484a43e57251bc378621ac87e
asbjornkallevik <88311284+asbjornkallevik@users.noreply.github.com>
2022-02-20 23:11:53 +0100

    Importing Next progress bars

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/styles.scss";
 import { AuthProvider } from "../context/AuthContext";
+import NextProgress from "next-progress";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <NextProgress />
       <Component {...pageProps} />
     </AuthProvider>
   );
